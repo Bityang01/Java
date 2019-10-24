047dff5a9862547a5ae1d7bd9fc40705f16dbf4d
Bityang01 <770545784@qq.com>
2019-10-24 22:46:40 +0800

    leetcode加一

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -97,3 +97,35 @@ class Solution {
         return sumMax;
     }
 }
+
+class Solution {
+	//20191024leetcode66加一
+    public int[] plusOne(int[] digits) {
+        int flg=0;
+        int count=digits.length+1;
+        int[] arr=new int[count];
+        for(int i=digits.length-1;i>=0;i--)
+        {
+            if(digits[i]!=9)
+            {
+                digits[i]=digits[i]+1;
+                for(int j=i+1;j<digits.length;j++)
+                {
+                    digits[j]=0;
+                }
+                flg=1;
+                return digits;
+            }
+        }
+        if(flg==0)
+        {
+            arr[0]=1;
+            for(int k=1;k<=digits.length;k++)
+            {
+                arr[k]=0;
+            }
+            return arr;
+        }
+        return digits;
+    }
+}
