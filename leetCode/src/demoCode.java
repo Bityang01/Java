44e9c72008a2e2d82e56c571211303d10953b7d1
Bityang01 <770545784@qq.com>
2019-10-27 23:15:44 +0800

    leetcode爬楼梯

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -147,3 +147,23 @@ class Solution {
         return 0;
     }
 }
+
+
+class Solution {
+	//20191027leetcode70爬楼梯
+    public int climbStairs(int n) {
+    if(n==1)
+    return 1;
+    if(n==2)
+    return 2;
+    int f1=1;
+    int f2=2;
+    int f3=0;
+    for(int i=3;i<=n;i++){
+        f3=f1+f2;
+        f1=f2;
+        f2=f3;
+    }
+    return f3;
+    }
+}
