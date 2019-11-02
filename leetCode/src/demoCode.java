2be113b6c7a3bc6cdd9280c56f1d7984a92a3902
Bityang01 <770545784@qq.com>
2019-11-02 16:07:53 +0800

    leetcode环形链表I

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -316,3 +316,32 @@ public class Solution {
         return null;
     }
 }
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+	//20191102leetcode141环形链表I给定一个链表，判断链表中是否有环。
+	//为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环
+    public boolean hasCycle(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null&&fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
