81cefa02b7186d266d565b7460e750f79efdd667
Bityang01 <770545784@qq.com>
2019-11-04 22:53:43 +0800

    leetcode移除链表元素

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -473,3 +473,37 @@ class Solution {
         return node.next;
     }
 }
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+	//20191104leetcode203移除链表元素
+    public ListNode removeElements(ListNode head, int val) {
+        if(head==null){
+            return null;
+        }
+        ListNode prev=head;
+        ListNode cur=head.next;
+        while(cur!=null){
+            if(cur.val==val){
+                prev.next=cur.next;
+                cur=cur.next;
+            }else{
+                prev=cur;
+                cur=cur.next;
+            }
+        }
+        if(head.val==val)
+        {
+           head=head.next;
+        }
+        return head;
+    }
+}
