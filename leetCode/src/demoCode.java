eada35a3b84ef589326a3567320ac4f0045169fb
Bityang01 <770545784@qq.com>
2019-11-07 19:21:00 +0800

    leetcode删除排序链表中的重复元素

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -692,3 +692,36 @@ class Solution {
         return BeforStart;
     }
 }
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+	//20191107leetcode82删除排序链表中的重复元素II（不包含重复元素）
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode cur=head;
+        ListNode node=new ListNode(-1);
+        ListNode tmp=node;
+        while(cur!=null){
+            if(cur.next!=null&&cur.val==cur.next.val){
+                while(cur.next!=null&&cur.val==cur.next.val){
+                    cur=cur.next;
+                }
+                cur=cur.next;
+            }else{
+                tmp.next=cur;
+                tmp=tmp.next;
+                cur=cur.next;
+                tmp.next=null;
+            }
+        }
+        return node.next;
+    }
+}
