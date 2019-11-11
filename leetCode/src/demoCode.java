ff738ad9037dca110bcd6273440c1c78f95fddbd
Bityang01 <770545784@qq.com>
2019-11-11 17:26:30 +0800

    leetcode反转字符串

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -935,3 +935,15 @@ class Solution {
         return true;
     }
 }
+
+
+class Solution {
+	//20191111leetcode344反转字符串
+    public void reverseString(char[] s) {
+        for(int i=0,j=s.length-1;i<=j;i++,j--){
+            char c=s[i];
+            s[i]=s[j];
+            s[j]=c;
+        }
+    }
+}
