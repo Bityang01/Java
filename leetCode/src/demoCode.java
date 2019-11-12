011aaf8137fe2906514258b58bd3c47d2e367646
Bityang01 <770545784@qq.com>
2019-11-12 16:40:52 +0800

    leetcode位1的个数

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -968,3 +968,18 @@ class Solution {
         return 0;
     }
 }
+
+
+public class Solution {
+	//20191112leetcode191位1的个数
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count=0;
+        for(int i=0;i<32;i++){
+            if(((n>>i)&1)==1){
+                count++;
+            }
+        }
+        return count;
+    }
+}
