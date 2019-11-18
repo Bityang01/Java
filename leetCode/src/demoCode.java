fd5f5383d9b2166a4524363fdd84b80e9dd94f20
Bityang01 <770545784@qq.com>
2019-11-18 21:50:21 +0800

    leetcode最大连续1的个数

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -1231,3 +1231,22 @@ class Solution {
         return false;
     }
 }
+
+
+class Solution {
+	//20191118leetcode485最大连续1的个数
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max=0;
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                count++;
+            }
+            if(nums[i]==0){
+                max=max>count?max:count;
+                count=0;
+            }
+        }
+        return max>count?max:count;
+    }
+}
