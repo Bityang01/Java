785e56861ed4349e3b1ef63f9a937047fea2cf8c
Bityang01 <770545784@qq.com>
2019-11-20 19:40:37 +0800

    leetcode第N个泰波那契数

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -1338,3 +1338,26 @@ class Solution {
         return sumBig-sumLow;
     }
 }
+
+
+class Solution {
+	//20191120leetcode1137第N个泰波那契数
+    public int tribonacci(int n) {
+        if(n==0)
+            return 0;
+        if(n==1||n==2){
+            return 1;
+        }
+        int T0=0;
+        int T1=1;
+        int T2=1;
+        int T3=0;
+        for(int i=3;i<=n;i++){
+            T3=T0+T1+T2;
+            T0=T1;
+            T1=T2;
+            T2=T3;
+        }
+        return T3;
+    }
+}
