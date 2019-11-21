f8612d1e780f63b5860b48e15097331c4315822f
Bityang01 <770545784@qq.com>
2019-11-21 22:57:15 +0800

    leetcode二分查找

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -1361,3 +1361,23 @@ class Solution {
         return T3;
     }
 }
+
+
+class Solution {
+	//20191121leetcode704二分查找
+    public int search(int[] nums, int target) {
+        int right=nums.length-1;
+        int left=0;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(nums[mid]>target){
+                right=mid-1;
+            }else if(nums[mid]<target){
+                left=mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
