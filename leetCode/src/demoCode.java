3ddcd95aa11b6e98a8169bdfd65421d8f9a7039f
Bityang01 <770545784@qq.com>
2020-01-14 20:05:56 +0800

    leetcode范围求和II

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -3397,3 +3397,25 @@ class Solution {
         return true;
     }
 }
+
+
+//思路：其实此题有个bug，因为每次ops，必定是从[0][0]左上角开始的，那么只需要找到最小的a和b便可以
+class Solution {
+	//20200114leetcode598范围求和II
+    public int maxCount(int m, int n, int[][] ops) {
+        if(ops.length==0){
+            return m*n;
+        }
+        int minA=ops[0][0];
+        int minB=ops[0][1];
+        for(int i=0;i<ops.length;i++){
+            if(ops[i][0]<minA){
+                minA=ops[i][0];
+            }
+            if(ops[i][1]<minB){
+                minB=ops[i][1];
+            }
+        }
+        return minA*minB;
+    }
+}
