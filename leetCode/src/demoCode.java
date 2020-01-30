04a7f4a1e2a635f8b70d423892d9e010f0d93b4f
Bityang01 <770545784@qq.com>
2020-01-30 18:33:09 +0800

    leetcode二叉树的右视图

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -3913,3 +3913,40 @@ class Solution {
         return l;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+	//20200130leetcode199二叉树的右视图
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null){
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i=0;i<size;i++){
+                TreeNode node = queue.poll();
+                if(i == (size-1)){
+                    res.add(node.val);
+                }
+                if(node.left != null){
+                    queue.add(node.left);
+                }
+                if(node.right != null){
+                    queue.add(node.right);
+                }
+            }
+        }
+        return res;
+    }
+}
