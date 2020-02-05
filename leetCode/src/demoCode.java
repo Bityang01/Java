70785d625cfceee3a47d18b31756330e5a9a0560
Bityang01 <770545784@qq.com>
2020-02-05 18:12:30 +0800

    leetcode字符串的最大公因子

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -4116,3 +4116,20 @@ class Solution {
         return Arrays.copyOfRange(sArr,0,count);
     }
 }*/
+
+
+
+class Solution {
+	//20200205leetcode1071字符串的最大公因子
+    public String gcdOfStrings(String str1, String str2) {
+        if((str1+str2).equals(str2+str1)){
+            for(int i=str1.length();i>0;i--){
+                if(str1.length()%i==0&&str2.length()%i==0
+                &&(str1.substring(0,i)+str2).equals(str2+str1.substring(0,i))){
+                    return str1.substring(0,i);
+                }
+            }
+        }
+        return "";
+    }
+}
