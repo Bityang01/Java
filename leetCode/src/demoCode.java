a02c0f234225676eca0ae01fb3829dea6caf2b51
Bityang01 <770545784@qq.com>
2020-02-07 15:05:04 +0800

    leetcode寻找比目标字母大的最小字母

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -4150,3 +4150,21 @@ class Solution {
         return false;
     }
 }
+
+
+class Solution {
+	//20200207leetcode744寻找比目标字母大的最小字母
+    public char nextGreatestLetter(char[] letters, char target) {
+        int left = 0;
+        int right = letters.length - 1;
+        while(left <= right){
+            int mid = (left+right) /2;
+            if(target < letters[mid]){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return letters[left%letters.length];
+    }
+}
