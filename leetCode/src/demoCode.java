c57c96db7ce25cedbcfc606f8de888c937802ae6
Bityang01 <770545784@qq.com>
2020-02-15 18:36:41 +0800

    leetcode阶乘后的零

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -4409,3 +4409,15 @@ class Solution {
         return i-1;
     }
 }
+
+class Solution {
+	//20200215leetcode172阶乘后的零
+    public int trailingZeroes(int n) {
+        int count = 0;
+        while(n > 0){
+            count += n/5;
+            n = n/5;
+        }
+        return count;
+    }
+}
