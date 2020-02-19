bbe1fbe973d6b219b10d08052379ff2ff10a6486
Bityang01 <770545784@qq.com>
2020-02-19 18:33:15 +0800

    leetcode完美数

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -4483,3 +4483,20 @@ class Solution {
         return (double)max/k;
     }
 }
+
+
+class Solution {
+	//20200219leetcode507完美数
+    public boolean checkPerfectNumber(int num) {
+        if(num<=1){
+            return false;
+        }
+        int sum = 0;
+        for(int i=1;i*i<=num;i++){
+            if((num%i)==0){
+                sum += i + num/i;
+            }
+        }
+        return sum-num == num;
+    }
+}
