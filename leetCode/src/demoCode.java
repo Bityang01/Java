90a5578fc256ac3c6c342690421fe0ed1262421a
Bityang01 <770545784@qq.com>
2020-03-03 20:59:58 +0800

    leetcode二叉树的最大深度

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -4815,3 +4815,25 @@ class Solution {
         Arrays.sort(A);
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+	//20200303leetcode104二叉树的最大深度
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+        return leftHeight>rightHeight?leftHeight+1:rightHeight+1;
+    }
+}
