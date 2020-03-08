0fa85270493793f989be6e4e3c4052c6f0bda000
Bityang01 <770545784@qq.com>
2020-03-08 17:14:40 +0800

    leetcode有多少小于当前数字的数字

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -4949,3 +4949,21 @@ class Solution {
         return count;
     }
 }
+
+
+class Solution {
+	//20200308leetcode1365有多少小于当前数字的数字
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] res = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            int count=0;
+            for(int j = 0;j<nums.length;j++){
+                if(nums[j]<nums[i]){
+                    count++;
+                    res[i]=count;
+                }
+            }
+        }
+        return res;
+    }
+}
