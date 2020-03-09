038aae640ddd19e9c062ea8d63904c1f48f6d4d2
Bityang01 <770545784@qq.com>
2020-03-09 16:51:08 +0800

    leetcode将数字变成0的操作次数

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -4967,3 +4967,20 @@ class Solution {
         return res;
     }
 }
+
+
+class Solution {
+	//20200309leetcode1342将数字变成0的操作次数
+    public int numberOfSteps (int num) {
+        int count = 0;
+        while(num>0){
+            if(num%2==0){
+                num = num/2;
+            }else{
+                num -= 1;
+            }
+            count++;
+        }
+        return count;
+    }
+}
