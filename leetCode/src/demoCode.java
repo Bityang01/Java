6b40c1a985f81b5c6fe2dda5118ab8d7d48761c9
Bityang01 <770545784@qq.com>
2020-03-25 17:11:41 +0800

    leetcode面试题17. 打印从1到最大的n位数

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -5395,3 +5395,15 @@ class Solution {
         return b;
     }
 }
+
+
+class Solution {
+	//20200325leetcode面试题17. 打印从1到最大的n位数
+    public int[] printNumbers(int n) {
+        int[] res = new int[(int)Math.pow(10,n)-1];
+        for(int i = 0;i<res.length;i++){
+            res[i]=i+1;
+        }
+        return res;
+    }
+}
