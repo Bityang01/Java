fc8c071cb3b698d9290fa108948b97ff32cbd7a8
Bityang01 <770545784@qq.com>
2020-03-29 14:33:22 +0800

    leetcode替换空格

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -5551,3 +5551,19 @@ class Solution {
         node.next = null;
     }
 }
+
+
+class Solution {
+	//20200329leetcode替换空格
+    public String replaceSpace(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==' '){
+                sb.append("%20");
+            }else{
+                sb.append(s.charAt(i));
+            }
+        } 
+        return sb.toString();
+    }
+}
