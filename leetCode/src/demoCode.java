99010a3f14cb936e2e5ce44ed1bf355c6873bfe6
Bityang01 <770545784@qq.com>
2020-03-30 09:53:37 +0800

    leetcode圆圈中最后剩下的数字

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -5628,3 +5628,21 @@ public class Finder {
         return 0;
     }
 }
+
+
+class Solution {
+	//20200330leetcode面试题62.圆圈中最后剩下的数字
+    public int lastRemaining(int n, int m) {
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            list.add(i);
+        }
+        int index = 0;
+        while(n>1){
+            index = (index + m - 1) % n;
+            list.remove(index);
+            n--;
+        }
+        return list.get(0);
+    }
+}
