f31259e6e0ebf413a13f224a70e06a58038e6f34
Bityang01 <770545784@qq.com>
2020-04-11 18:11:19 +0800

    nowcoder每日一题：无缓存交换

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -6249,3 +6249,17 @@ public class Solution {
         return res;
     }
 }
+
+
+import java.util.*;
+
+public class Exchange {
+	//20200411nowcoder每日一题：无缓存交换
+    public int[] exchangeAB(int[] AB) {
+        // write code here
+        AB[0]=AB[0]^AB[1];
+        AB[1]=AB[0]^AB[1];
+        AB[0]=AB[0]^AB[1];
+        return AB;
+    }
+}
