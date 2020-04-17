60af93daf51ecb2d7b0222e8b3bc35d996ced265
Bityang01 <770545784@qq.com>
2020-04-17 16:58:33 +0800

    leetcode二叉树镜像

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -6546,3 +6546,28 @@ public class Main{
         }
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+	//20200417leetcode二叉树的镜像
+    public TreeNode mirrorTree(TreeNode root) {
+        if(root == null){
+            return root;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        mirrorTree(root.left);
+        mirrorTree(root.right);
+        return root;
+    }
+}
