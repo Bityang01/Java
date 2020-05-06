09a8788eb1bae826687bc2dc7dcb0dd4389e681f
Bityang01 <770545784@qq.com>
2020-05-06 19:16:30 +0800

    leetcodeLCP 06. 拿硬币

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -7370,3 +7370,19 @@ class Solution {
         return null;
     }
 }
+
+
+class Solution {
+	//20200506leetcodeLCP 06. 拿硬币
+    public int minCount(int[] coins) {
+        int count = 0;
+        for(int i=0;i<coins.length;i++){
+            if(coins[i]%2==0){
+                count += coins[i]/2;
+            }else{
+                count += (coins[i]/2) + 1;
+            }
+        }
+        return count;
+    }
+}
