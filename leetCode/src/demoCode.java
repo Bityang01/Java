260ff6af2e37a9d6a38c38d9e58388fb3d551aae
Bityang01 <770545784@qq.com>
2020-05-08 21:17:06 +0800

    nowcoder每日一题：数字和为sum的方法数

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -7420,3 +7420,30 @@ public class Main{
         }
     }
 }
+
+
+// 本题为考试单行多行输入输出规范示例，无需提交，不计分。
+import java.util.Scanner;
+
+public class Main {
+	//20200508nowcoder每日一题：数字和为sum的方法数
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {// 注意，如果输入是多个测试用例，请通过while循环处理多个测试用例
+            int n = in.nextInt();
+            int sum = in.nextInt();
+            int[] value = new int[n];
+            long[] dp = new long[sum + 1];
+            dp[0] = 1;
+            for (int i = 0; i < n; i++) {
+                value[i] = in.nextInt();
+                for (int j = sum; j >= 0; j--) {
+                    if (j >= value[i]) {
+                        dp[j] += dp[j - value[i]];
+                    }
+                }
+            }
+            System.out.println(dp[sum]);
+        }
+    }
+}
