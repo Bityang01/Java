b63616f7720155c9eb35ee97a1ad643c4b1a8019
Bityang01 <770545784@qq.com>
2020-05-13 18:22:59 +0800

    nowcoder每日一题：2的个数

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -7548,3 +7548,35 @@ public class Main{
         }
     }
 }
+
+
+import java.util.*;
+
+public class Count2 {
+	//20200513nowcoder每日一题：2的个数
+    public int countNumberOf2s(int n) {
+        int count = 0;
+        for(int i=1;i<=n;i *= 10){
+            int a = n / i,b = n % i;
+            count += (a + 7) / 10 * i + ((a % 10 == 2) ? b + 1 : 0);
+        }
+        return count;
+    }
+}
+/*
+public class Count2 {
+    public int countNumberOf2s(int n) {
+        // write code here
+        int count = 0;
+        for(int i =0;i<=n;i++){
+            int tmp = i;
+            while(tmp>0){
+                if(tmp%10==2){
+                    count++;
+                }
+                tmp /= 10;
+            }
+        }
+        return count;
+    }
+}*/
