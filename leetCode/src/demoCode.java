ac24f06419ef34d53d994f893d7854df38652bac
Bityang01 <770545784@qq.com>
2020-05-17 22:27:49 +0800

    nowcoder每日一题：字母统计

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -7681,3 +7681,26 @@ public class Solution {
         return secondSell;
     }
 }
+
+
+// 本题为考试单行多行输入输出规范示例，无需提交，不计分。
+import java.util.Scanner;
+
+public class Main {
+	//20200516nowcoder每日一题：字母统计
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {// 注意，如果输入是多个测试用例，请通过while循环处理多个测试用例
+            String str = in.nextLine();
+            int[] arr = new int[128];
+            for(int i=0;i<str.length();i++){
+                char tmp = str.charAt(i);
+                arr[tmp]++;
+            }
+            for(int i=65;i<91;i++){
+                char ch = (char)i;
+                System.out.println(ch+":"+arr[i]);
+            }
+        }
+    }
+}
