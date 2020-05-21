241d8e72ba4dc862c4e7f6740b33fc93ba119582
Bityang01 <770545784@qq.com>
2020-05-21 11:42:04 +0800

    nowcoder每日一题：删除公共字符

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -7915,3 +7915,24 @@ public class Main{
         }
     }
 }*/
+
+
+
+// 本题为考试单行多行输入输出规范示例，无需提交，不计分。
+import java.util.Scanner;
+
+public class Main {
+	//20200521nowcoder每日一题：删除公共字符
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {// 注意，如果输入是多个测试用例，请通过while循环处理多个测试用例
+            String str1 = in.nextLine();
+            String str2 = in.nextLine();
+            for(int i=0;i<str2.length();i++){
+                String tmp = "" + str2.charAt(i);
+                str1 = str1.replaceAll(tmp,"");
+            }
+            System.out.println(str1);
+        }
+    }
+}
