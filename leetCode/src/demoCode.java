2293c0109e07e8bc81fc11b9261e3d67621d6939
Bityang01 <770545784@qq.com>
2020-05-21 11:42:57 +0800

    nowcoder每日一题：句子逆序

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -7936,3 +7936,32 @@ public class Main {
         }
     }
 }
+
+
+
+// 本题为考试单行多行输入输出规范示例，无需提交，不计分。
+import java.util.Scanner;
+
+public class Main {
+	//20200521nowcoder每日一题：句子逆序
+    public static String reverse(String sentence){
+        sentence = " "+sentence;
+        StringBuilder sb = new StringBuilder();
+        int index = sentence.length();
+        for(int i=sentence.length()-1;i>=0;i--){
+            if(sentence.charAt(i) == ' '){
+                sb.append(sentence.substring(i+1,index));
+                sb.append(" ");
+                index = i;
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {// 注意，如果输入是多个测试用例，请通过while循环处理多个测试用例
+            String str = in.nextLine();
+            System.out.println(reverse(str));
+        }
+    }
+}
