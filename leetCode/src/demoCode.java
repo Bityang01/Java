cc5da1594675db8082617747cdd4ee03e2e7f704
Bityang01 <770545784@qq.com>
2020-05-25 12:18:22 +0800

    nowcoder每日一题：求和

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -8074,3 +8074,32 @@ public class Main {
         }
     }
 }
+
+
+import java.util.*;
+
+public class Main{
+	//20200525nowcoder每日一题：求和
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        String res = "";
+        int sum = 0; 
+        int[] num = new int[n];
+        int j =1;
+        for(int i = 0;i < n;i++)
+              num[i] = j++;
+        process(num,m,res,sum,0);
+    }
+     
+    public static void process(int[] num,int m,String res,int sum,int i){
+        if(i == num.length){
+            if(sum == m)
+                System.out.println(res.trim());
+            return;
+        }
+        process(num,m,res+num[i]+" ",sum+num[i],i+1);
+        process(num,m,res,sum,i+1);
+    }
+}
