84504c1ab3737708c9965e1d673716c506380d8d
Bityang01 <770545784@qq.com>
2020-06-12 18:57:41 +0800

    nowcoder web测试单调栈结构

--- leetCode/src/demoCode.java
+++ leetCode/src/demoCode.java
@@ -8241,3 +8241,40 @@ public class Main {
         }
     }
 }*/
+
+
+import java.util.*;
+
+public class Main{
+	//20200612nowcoder web测试单调栈结构
+    private static void left(int[] array,int i){
+        for(int j=i-1;j>=0;j--){
+            if(array[j] < array[i]){
+                System.out.print(j+" ");
+                return ;
+            }
+        }
+        System.out.print(-1+" ");
+    }
+    private static void right(int[] array,int i){
+        for(int j=i+1;j<array.length;j++){
+            if(array[j] < array[i]){
+                System.out.println(j);
+                return ;
+            }
+        }
+        System.out.println(-1);
+    }
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] array = new int[n];
+        for(int i=0;i<n;i++){
+            array[i] = in.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            left(array,i);
+            right(array,i);
+        }
+    }
+}
